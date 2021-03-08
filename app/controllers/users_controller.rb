a484d9cde2ba80b5853529a847347c3784ccd88e
akf38 <dpv28779@gmail.com>
2021-03-08 10:48:36 +0000

    Add follow and favorite function

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,7 +5,7 @@ class UsersController < ApplicationController
   
   def show
     @user = User.find(params[:id])
-    # グラフ描画のためのデータ。user_idに該当するものを、最新30件取得。各データを時間表示をchartkick指定にあわせて、再度配列化。
+    # グラフ描画のためのデータ。user_idに該当するものを、最新30件取得。各データ(体重、体脂肪率)を時間表示をchartkick指定にあわせて、再度配列化。
     data = UserInfo.where(user_id: @user.id).order(created_at: :desc).limit(30)
     @weight_data = data.select(:created_at, :weight).map { |d| [d.created_at.strftime('%Y%m%d').to_s, d.weight] }
     @body_fat_percentage_data = data.select(:created_at, :body_fat_percentage).map { |d| [d.created_at.strftime('%Y%m%d').to_s, d.body_fat_percentage] }
@@ -29,8 +29,24 @@ class UsersController < ApplicationController
     end
   end
   
+  def following
+    @user = User.find(params[:id])
+    @title = "#{@user.name}さんのフォロー中リスト"
+    @users = @user.following
+    render 'follow_follower_list'
+  end
+  
+  def followers
+    @user = User.find(params[:id])
+    @title = "#{@user.name}さんのフォロワーリスト"
+    @users = @user.followed
+    render 'follow_follower_list'
+  end
+  
   private
+  
   def user_params
     params.require(:user).permit(:tall, :weight, :body_fat_percentage, :image_id)
   end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,12 +6,18 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'homes#top'
   resources :users, only: [:index, :show, :edit, :update] do
+    member do
+      get :following, :followers
+    end
     resources :user_infos, only:[:index, :edit, :update, :destroy]
   end
   resources :records, only: [:index, :show, :edit, :update, :destroy, :create, :new] do
     resources :training_records, only: [:create, :update, :destroy]
   end
   post 'training_records/new_record_create' => 'training_records#new_record_create'
-  resources :tweets, only: [:index, :show, :edit, :update, :create]
+  resources :tweets, only: [:index, :show, :edit, :update, :create] do
+    resources :favorites, only: [:create, :destroy]
+  end
+  resources :relationships, only: [:create, :destroy]
 end
 
