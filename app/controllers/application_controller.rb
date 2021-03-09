b527b18b4878256c93517735d1d9c01fe4cc9e2c
akf38 <dpv28779@gmail.com>
2021-03-09 14:45:48 +0900

    Add kintorehistory(user_start_datecolumn)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,7 @@ class ApplicationController < ActionController::Base
   protected
   # デバイス用のストロングパラメータ
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :image_id, :profile, :weight, :tall, :body_fat_percentage])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :image_id, :profile, :weight, :tall, :body_fat_percentage, :start_date])
     devise_parameter_sanitizer.permit(:account_update, keys: [:name, :image, :profile])
   end
 end
