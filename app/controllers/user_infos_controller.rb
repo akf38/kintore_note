4b70eaf47abfde57edd9d932fc453a4fa60d65b0
akf38 <dpv28779@gmail.com>
2021-03-25 10:36:25 +0900

    Add 体型推移グラフ (体型一覧view)

--- app/controllers/user_infos_controller.rb
+++ app/controllers/user_infos_controller.rb
@@ -5,6 +5,14 @@ class UserInfosController < ApplicationController
   def index
     @user = User.find(params[:user_id])
     @user_infos = @user.user_infos.order(created_at: :desc).page(params[:page]).per(10)
+    
+    # グラフ描画のためのデータ。user_idに該当するものを、最新30件取得。各データ(体重、体脂肪率)を時間表示をchartkick指定にあわせて、再度配列化。
+    data = UserInfo.where(user_id: @user.id).order(created_at: :desc).limit(30)
+    weight_data = data.select(:created_at, :weight)
+    @weight_data = weight_data.map { |d| [d.created_at.strftime('%Y%m%d').to_s, d.weight] }
+    bfp_data = data.select(:created_at, :body_fat_percentage)
+    @bfp_data = bfp_data.map { |d| [d.created_at.strftime('%Y%m%d').to_s, d.body_fat_percentage] }
+
   end
 
   def update
