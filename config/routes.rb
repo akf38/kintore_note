aafed6aaf043402c412a9566b5e4fb6d323577da
akf38 <dpv28779@gmail.com>
2021-03-31 16:26:29 +0900

    Add ゲストログイン機能

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,9 @@ Rails.application.routes.draw do
     :sessions => 'users/sessions',
     :omniauth_callbacks => 'users/omniauth_callbacks',
   }
+  devise_scope :user do
+    post 'users/guest_sign_in', to: 'users/sessions#guest_sign_in'
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'homes#top'
   get '/privacy' => 'homes#privacy'
